d8e58f3abfe1cec352e5e1f1b9e9d344ce6822b0
DanielBR <daniel.benito@iisi.com.mx>
2024-11-28 13:04:45 -0600

    Se realizó una modificación en la sección de artículos, aumentando el tamaño de los precios de cada uno de ellos para mejorar su visibilidad.

--- OnlineStore.UI/Models/Product.cs
+++ OnlineStore.UI/Models/Product.cs
@@ -15,12 +15,23 @@ namespace OnlineStore.UI.Models
         public int Stock { get; set; }
         public string ImageUrl { get; set; }
         public Guid ProductTypeId { get; set; }
+        public bool IsButtonEnabled { get { return OnStock(Stock); } }
 
         public ProductType ProductType { get; set; }
 
         public string OnPrice(double? price)
         {
             string s = "$" + price.ToString();
+            return s;
+        }
+
+        public bool OnStock(int Stock)
+        {
+            bool s = true;
+            if (Stock == 0)
+            {
+                s = false;
+            }
             return s;
         }
     }
